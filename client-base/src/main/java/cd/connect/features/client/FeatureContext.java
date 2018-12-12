1f2392bfbbe79c47ef9d9197d99f0bdefb4ce21c
Richard Vowles <richard@bluetrainsoftware.com>
2018-12-12 13:36:49 +1300

    - add developer overrides and tests for them - add name in pom.xml - add missing descriptions

--- client-base/src/main/java/cd/connect/features/client/FeatureContext.java
+++ client-base/src/main/java/cd/connect/features/client/FeatureContext.java
@@ -11,14 +11,12 @@ import java.util.Optional;
  */
 public class FeatureContext {
 	public static final String ACCELERATE_FEATURE_OVERRIDE = "accelerate-feature-override";
+	static final String FEATURE_TOGGLES_ALLOW_OVERRIDE = "feature-toggles.allow-override";
+	public static final String FEATURE_TOGGLES_PREFIX = "feature-toggles.";
 	public static FeatureRepository repository;
 
   public static boolean isActive(Feature feature) {
-    if (repository == null) {
-      throw new RuntimeException("You must configure your feature repository before using it.");
-    }
-
-    if (System.getProperty("feature-toggles.allow-override") != null ) {
+    if (System.getProperty(FEATURE_TOGGLES_ALLOW_OVERRIDE) != null ) {
 	    String override = Optional.ofNullable(GlobalTracer.get())
 		    .map(Tracer::activeSpan)
 		    .filter(Objects::nonNull)
@@ -34,6 +32,14 @@ public class FeatureContext {
 	    }
     }
 
+    String devOverride = System.getProperty(FEATURE_TOGGLES_PREFIX + feature.name());
+    if (devOverride != null) {
+    	return Boolean.parseBoolean(devOverride);
+    }
+
+	  if (repository == null) {
+		  throw new RuntimeException("You must configure your feature repository before using it.");
+	  }
 //		  .map(override -> ).orElse(false);
 
     FeatureState fs = repository.getFeatureState(feature.name());
--- client-base/src/test/java/cd/connect/features/client/FeatureContextTest.java
+++ client-base/src/test/java/cd/connect/features/client/FeatureContextTest.java
@@ -3,11 +3,14 @@ package cd.connect.features.client;
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
 
 import static cd.connect.features.client.FeatureContext.ACCELERATE_FEATURE_OVERRIDE;
+import static cd.connect.features.client.FeatureContext.FEATURE_TOGGLES_ALLOW_OVERRIDE;
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
@@ -23,6 +26,23 @@ public class FeatureContextTest {
 		public boolean isActive() {
 			return FeatureContext.isActive(this);
 		}
+	}
+
+	static Tracer tracer;
+
+	@BeforeClass
+	public static void init() {
+		// we can't re-register this one
+		tracer = mock(Tracer.class);
+		GlobalTracer.register(tracer);
+	}
+
+	@Before
+	public void setup() {
+		System.clearProperty(FEATURE_TOGGLES_ALLOW_OVERRIDE);
+		System.clearProperty(FeatureContext.FEATURE_TOGGLES_PREFIX + SampleFeatures.mary.name());
+		System.clearProperty(FeatureContext.FEATURE_TOGGLES_PREFIX + SampleFeatures.john.name());
+		FeatureContext.repository = null;
 	}
 
 	@Test
@@ -43,15 +63,29 @@ public class FeatureContextTest {
 	}
 
 	@Test
+	public void testDeveloperOverrides() {
+		System.setProperty(FeatureContext.FEATURE_TOGGLES_PREFIX + SampleFeatures.mary.name(), "true");
+		System.setProperty(FeatureContext.FEATURE_TOGGLES_PREFIX + SampleFeatures.john.name(), "false");
+		assertThat(SampleFeatures.mary.isActive()).isTrue();
+		assertThat(SampleFeatures.john.isActive()).isFalse();
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void noOverrideAnNoTraceFails() {
+
+		assertThat(SampleFeatures.john.isActive()).isFalse();
+	}
+
+	@Test
 	public void testOpenTracingOverride() {
 		FeatureContext.repository = mock(FeatureRepository.class);
 
-		Tracer tracer = getTracer();
+//		Tracer tracer = getTracer();
 		
 		Span span = mock(Span.class);
 		when(tracer.activeSpan()).thenReturn(span);
 
-		System.setProperty("feature-toggles.allow-override", "true");
+		System.setProperty(FEATURE_TOGGLES_ALLOW_OVERRIDE, "true");
 
 		// override so mary is active, john is disabled
 		when(span.getBaggageItem(eq(ACCELERATE_FEATURE_OVERRIDE))).thenReturn(String.format("%s=true%s=false",
@@ -60,10 +94,8 @@ public class FeatureContextTest {
 		// mary is locked, john is enabled
 		FeatureState mary = new FeatureState();
 		mary.locked = true;
-		when(FeatureContext.repository.getFeatureState(eq(SampleFeatures.mary.name()))).thenReturn(mary);
 		FeatureState john = new FeatureState();
 		john.whenEnabled = LocalDateTime.now();
-		when(FeatureContext.repository.getFeatureState(eq(SampleFeatures.john.name()))).thenReturn(john);
 
 
 		assertThat(SampleFeatures.mary.isActive()).isTrue();
@@ -87,7 +119,7 @@ public class FeatureContextTest {
 	public void noPropertyMeansNoTracingOverride() {
 		FeatureContext.repository = mock(FeatureRepository.class);
 
-		Tracer tracer = getTracer();
+//		Tracer tracer = getTracer();
 
 		Span span = mock(Span.class);
 		when(tracer.activeSpan()).thenReturn(span);
@@ -100,6 +132,6 @@ public class FeatureContextTest {
 		FeatureState mary = new FeatureState();
 		mary.locked = true;
 		when(FeatureContext.repository.getFeatureState(eq(SampleFeatures.mary.name()))).thenReturn(mary);
-		assertThat(SampleFeatures.mary.isActive()).isTrue();
+		assertThat(SampleFeatures.mary.isActive()).isFalse();
 	}
 }
